95f54559bdec06d170e8c7788a7f461f70c99c9a
Marcelo Rodriguez <mrodriguezm9@est.ups.edu.ec>
2025-06-09 16:44:35 -0500

    Variables y Consola

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//Función para mostrar un cuadro de Diálogo Emergente
-
-function mostrarMensaje(){
+// Función para mostrar un cuadro de diálogo emergente
+function mostrarMensaje() {
     alert("Hola Mundo :)");
+}
+
+// VARIABLES
+function imprimirVariables() {
+    let nombre = "Marcelo";
+    let edad = "22";
+
+    console.log("nombre:", nombre);
+    console.log("edad:", edad);
+
+    // Forma correcta de concatenar cadenas usando template literals
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+
+    console.error("Error: BLA BLA BLA");
+    console.warn("Advertencia: x x x");
 }
